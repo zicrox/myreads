380031113d7828904f4b4ca5696539ec85833bec
Miguel Díaz <ademouse@gmail.com>
2017-10-14 15:50:25 +0200

    Convert BookshelfChanger to a stateless functional component

--- src/Book.js
+++ src/Book.js
@@ -1,18 +1,11 @@
 import React from 'react'
+import BookshelfChanger from './BookshelfChanger'
 
 const Book = (props) => (
   <div className="book">
     <div className="book-top">
       <div className="book-cover" style={props.bookData.cover}></div>
-      <div className="book-shelf-changer">
-        <select>
-          <option value="none" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
+      <BookshelfChanger />
     </div>
     <div className="book-title">{props.bookData.title}</div>
     <div className="book-authors">{props.bookData.authors}</div>
